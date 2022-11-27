6ad17bd278721ed3d40dafb23cad3d4e8f121ba5
Hower454516977 <454516977@qq.com>
2022-11-27 17:35:48 +0800

    change origin

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://socket-react-client.zeabur.app",
+    origin: "*",
     methods: ["GET", "POST"],
   },
 });
