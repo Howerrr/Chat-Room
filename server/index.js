272856c0c9c1030b8f9c7f4d8f7217f95ae9275c
Hower454516977 <454516977@qq.com>
2022-11-27 18:56:30 +0800

    update

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "http://chat-room-client.zeabur.app",
     methods: ["GET", "POST"],
   },
 });
@@ -31,6 +31,8 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3001, () => {
-  console.log("SERVER RUNNING");
+const port = process.env.PORT || 5000
+
+server.listen(port, () => {
+  console.log("SERVER RUNNING", port);
 });
