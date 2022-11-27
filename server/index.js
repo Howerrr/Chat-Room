4e9e7a181cc97d15b6b992e8cfefe9989e79cd36
Hower454516977 <454516977@qq.com>
2022-11-27 17:25:11 +0800

    update path

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://socket-react-client.zeabur.app",
     methods: ["GET", "POST"],
   },
 });
